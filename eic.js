4716c61aeb13a2677c5352110accdf5c6ab32a8c
Ármin Scipiades <armin.scipiades@egroup.hu>
2016-03-02 15:21:04 +0100

    Use module pattern less obscenely
    Also tell bower about it.

--- eic.js
+++ eic.js
@@ -1,6 +1,6 @@
 /* global _ */
 
-var EIC = (function() {
+(function(root) {
     var charValues = {
         '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
         'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15, 'g': 16, 'h': 17, 'i': 18, 'j': 19, 'k': 20, 'l': 21, 'm': 22, 'n': 23, 'o': 24, 'p': 25, 'q': 26, 'r': 27, 's': 28, 't': 29, 'u': 30, 'v': 31, 'w': 32, 'x': 33, 'y': 34, 'z': 35,
@@ -200,7 +200,7 @@ var EIC = (function() {
         return issuers[str.substr(0, 2)];
     };
 
-    return {
+    var EIC = {
         "mayBeEIC": mayBeEIC,
         "calcCheckChar": calcCheckChar,
         "isValid": isValid,
@@ -208,4 +208,6 @@ var EIC = (function() {
         "getIssuer": getIssuer,
         "examine": examine
     };
-})();
+
+    root.EIC = EIC;
+})(this);
